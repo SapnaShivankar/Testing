e519f46b443c21f689404397bed694569e0861fb
SapnaShivankar <appxbuild@appxbuilds-Mac-mini.local>
2017-05-23 11:53:18 +0530

    Newstud

---
+++ Odometer/app/src/main/java/com/appxbuild/odometer/OdometerService.java
@@ -0,0 +1,9 @@
+package com.appxbuild.odometer;
+
+import android.app.Service; import android.content.Intent; import android.os.IBinder;
+public class OdometerService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+//Code to bind the service
+    }
+}
