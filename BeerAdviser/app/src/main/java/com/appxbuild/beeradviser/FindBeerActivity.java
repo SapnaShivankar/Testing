fd805f31c9006d0fe4699711693bf2ef83ac0d5f
SapnaShivankar <appxbuild@appxbuilds-Mac-mini.local>
2017-05-23 11:38:19 +0530

    Beer Adviser

---
+++ BeerAdviser/app/src/main/java/com/appxbuild/beeradviser/FindBeerActivity.java
@@ -0,0 +1,40 @@
+package com.appxbuild.beeradviser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+ import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
+
+
+
+public class FindBeerActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+    //Call when the user clicks the button
+    public void onClickFindBeer(View view)
+    {
+
+//Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands); //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+//Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem()); //Display the selected item
+        brands.setText(beerType);
+
+        //Get recommendations from the BeerExpert class
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n'); }
+//Display the beers
+        brands.setText(brandsFormatted);
+    }
+}
+
